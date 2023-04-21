caa55e5501ef08a3321132ade631b7274aae6cbe
wastech <fataiwasiu2@gmail.com>
2023-04-21 03:51:14 +0100

    added more features :white_check_mark:

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from './pipes/validation.pipe';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import helmet from 'helmet';
 import * as compression from 'compression';
 import * as csurf from 'csurf';
@@ -8,10 +9,19 @@ import * as csurf from 'csurf';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  const config = new DocumentBuilder()
+  .setTitle('Blog API')
+  .setDescription('A Blog App API')
+  .setVersion('1.0')
+  .addTag('cats')
+  .build();
+const document = SwaggerModule.createDocument(app, config);
+SwaggerModule.setup('api', app, document)
+
   // Apply Helmet middleware to all requests
   app.use(helmet());
   app.use(compression());
-  app.use(csurf());
+  // app.use(csurf());
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
