dd0b476c59d6a3ee106ecbe1293975935cbc0df8
wastech <fataiwasiu2@gmail.com>
2023-04-21 03:31:22 +0100

    added more features :white_check_mark:

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from './pipes/validation.pipe';
 import { AppModule } from './app.module';
+import helmet from 'helmet';
+import * as compression from 'compression';
+import * as csurf from 'csurf';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors()
+
+  // Apply Helmet middleware to all requests
+  app.use(helmet());
+  app.use(compression());
+  app.use(csurf());
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
   console.log(`Application is running on: ${await app.getUrl()}`);
